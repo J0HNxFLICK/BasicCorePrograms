028b77a47ce6fc7d941e12f45b19f01ed838c41c
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 12:11:25 +0530

    [Arun] Added program that finds prime factors.

---
+++ Factors/Factors.cs
@@ -0,0 +1,31 @@
+﻿namespace Factors
+{
+    internal class Factors
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Factorial finder.");
+            int counter;
+
+            Console.WriteLine("Enter Nth value of which you want prime factorization for : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (counter = 2; n > 1; counter ++)
+            {
+                if (n % counter == 0)
+                {
+                    int tracker = 0;
+                    while (n % counter == 0)
+                    {
+                        n /= counter;
+                        tracker++;
+                    }
+
+                    Console.WriteLine("{0} is a prime factor {1} times." , counter , tracker);
+                }
+            }
+
+
+        }
+    }
+}
