69ef4f6a1ee480bce72d69fee5e982e6ec257721
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 14:40:46 +0530

    [Arun] Added power of 2 calculator program

---
+++ PowerOf2/PowerOf2.cs
@@ -0,0 +1,20 @@
+﻿namespace PowerOf2
+{
+    internal class PowerOf2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Power Of 2 calculator.");
+
+            double powerOf2 = 0;
+            Console.WriteLine("Enter power value : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int counter = 1 ; counter <= userInput; counter++)
+            {
+                powerOf2 = Math.Pow(2, counter);
+                Console.WriteLine("2 ^ {0} : {1}", counter , powerOf2);
+            }
+        }
+    }
+}
