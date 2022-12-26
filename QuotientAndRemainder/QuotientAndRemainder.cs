9eeedafc41d07a410ec8d6cc3d6c85919585dd84
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 14:42:11 +0530

    [Arun] Added quotient and remainder finder program

---
+++ QuotientAndRemainder/QuotientAndRemainder.cs
@@ -0,0 +1,21 @@
+﻿namespace QuotientAndRemainder
+{
+    internal class QuotientAndRemainder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quotiant and remainder.");
+
+            Console.WriteLine("Enter dividend : ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor : ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend/ divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is : " + quotient);
+            Console.WriteLine("Remainder is : " + remainder);
+        }
+    }
+}
