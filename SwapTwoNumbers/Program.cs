a014d7b8c67fdffda79a6ca5fed6f6075a37df5b
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 14:44:43 +0530

    [Arun] Added program that swaps given numbers

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SwapTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number swapper.");
+            int tempNumHolder = 0;
+
+            Console.WriteLine("Enter first number : ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            tempNumHolder = firstNum;
+            firstNum= secondNum;
+            secondNum= tempNumHolder;
+
+            Console.WriteLine("Now first number is : " + firstNum);
+            Console.WriteLine("And second number is : " + secondNum);
+        }
+    }
+}
