1f2d8d675e30e4b467a94176cf903cf6dcd850ff
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 12:23:17 +0530

    [Arun] Added program that checks greatest of 3 numbers.

---
+++ GreatestOf3Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GreatestOf3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find greatest of 3 numbers.");
+
+            Console.Write("Input the 1st number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the  2nd number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the 3rd  number : ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("The 1st Number is the greatest among three.");
+                }
+                else
+                {
+                    Console.Write("The 3rd Number is the greatest among three.");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("The 2nd Number is the greatest among three.");
+            else
+                Console.Write("The 3rd Number is the greatest among three.");
+        }
+    }
+}
