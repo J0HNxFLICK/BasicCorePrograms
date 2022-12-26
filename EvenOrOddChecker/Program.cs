283d486886bac17122dfec98f404f21d603da559
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 12:06:23 +0530

    [Arun] Added program that checks even or odd.

---
+++ EvenOrOddChecker/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EvenOrOddChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even or odd checker.");
+
+            Console.WriteLine("Enter a number to check if a number is even or odd : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput % 2 == 0)
+            {
+                Console.WriteLine(userInput + " is an even number.");
+            }
+            else
+            {
+                Console.WriteLine(userInput + " is an odd number.");
+            }
+        }
+    }
+}
