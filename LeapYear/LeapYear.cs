cf550dcd517fe77e18724379421b7808be31b77e
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-26 14:37:59 +0530

    [Arun] Added leap year program

---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,22 @@
+﻿namespace LeapYear
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leap year checker.");
+
+            Console.WriteLine("Enter a year to check if its a leap year : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput % 4 == 0 || userInput % 100 == 0 || userInput % 400 == 10)
+            {
+                Console.WriteLine(userInput + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(userInput + " is not a leap year");
+            }
+        }
+    }
+}
